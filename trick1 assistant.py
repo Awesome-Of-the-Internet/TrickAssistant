eff138758c59b5d4f3a5794285b561f83992ca54
Tricky <72969144+Tricky4747@users.noreply.github.com>
2021-09-30 09:49:46 +0530

    Update trick1 assistant.py

--- trick1 assistant.py
+++ trick1 assistant.py
@@ -1,4 +1,4 @@
-# This is made and copyrighted by Rithu. Hahahahahaha !!!!!
+# This is made and copyrighted by Tricky. Hahahahahaha !!!!!
 import pyttsx3
 import speech_recognition as sr
 import datetime
@@ -9,6 +9,7 @@ import smtplib
 import random
 import wolframalpha
 import sys
+from selenium import webdriver
 
 print("Initializing Trick...")
 engine = pyttsx3.init('sapi5')
@@ -16,7 +17,6 @@ client = wolframalpha.Client('Your_App_ID')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 song_number = random.randint(0, 0)
-
 
 # this function will pronounce the string in it.
 def speak(text):
@@ -28,14 +28,14 @@ def speak(text):
 def wish_me():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
-        speak('Good Morning Rithu')
-        print('Good Morning Rithu')
+        speak('Good Morning Tricky')
+        print('Good Morning Tricky')
     elif hour >= 12 and hour< 18:
-        speak('Good Afternoon Rithu')
-        print('Good Afternoon Rithu')
+        speak('Good Afternoon Tricky')
+        print('Good Afternoon Tricky')
     else:
-        speak('Good Evening Rithu')
-        print('Good Evening Rithu')
+        speak('Good Evening Tricky')
+        print('Good Evening Tricky')
 
 
 # this function will take command from the microphone.
@@ -44,14 +44,14 @@ def myCommand():
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
-        audio = r.listen(source)
+        audio = r.listen(source, phrase_time_limit=5)
     try:
         query = r.recognize_google(audio, language='en-in')
         print('User: ' + query + '\n')
 
     except sr.UnknownValueError:
-        speak('Sorry Rithu! I didn\'t get that! Try typing the query!')
-        print('Sorry Rithu! I didn\'t get that! Try typing the query!')
+        speak('Sorry Tricky! I didn\'t get that! Try typing the query!')
+        print('Sorry Tricky! I didn\'t get that! Try typing the query!')
         query = str(input('Query: '))
 
     return query
@@ -63,8 +63,8 @@ wish_me()
 speak('I am Trick. How may I help you?')
 
 # logic for executing basic tasks
-name = "Rithu"
-if name == 'Rithu':
+name =  Tricky"
+if name ==  Tricky':
 
     while True:
 
@@ -85,12 +85,13 @@ if name == 'Rithu':
             speak('okay')
             webbrowser.open('www.gmail.com')
 
+
         elif "what\'s up" in query or 'how are you' in query:
             stMsgs = ['I am fine!', 'Nice!', 'I am nice and full of energy']
             speak(random.choice(stMsgs))
 
         elif 'play music' in query:
-            songs_dir = "E:\\rithu songs"
+            songs_dir = "E:\\Tricky\ Tricky songs"
             songs = os.listdir(songs_dir)
             print(songs)
             os.startfile(os.path.join(songs_dir, songs[0]))
@@ -101,42 +102,23 @@ if name == 'Rithu':
 
         elif 'open timer' in query or 'open cstimer' in query or 'open CS timer' in query.lower():
             speak('okay')
-            chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-            webbrowser.get(chrome_path).open('www.cstimer.net')
+            brave_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+            webbrowser.get(brave_path).open('www.cstimer.net')
 
-        elif 'email' in query:
-            speak('Who is the recipient? ')
-            recipient = myCommand()
-
-            if 'me' in recipient:
-                try:
-                    speak('What should I say? ')
-                    content = myCommand()
-
-                    server = smtplib.SMTP('smtp.gmail.com', 587)
-                    server.ehlo()
-                    server.starttls()
-                    server.login("Your_Username", 'Your_Password')
-                    server.sendmail('Your_Username', "Recipient_Username", content)
-                    server.close()
-                    speak('Email sent!')
-
-                except:
-                    speak('Sorry Sir! I am unable to send your message at this moment!')
 
         elif 'nothing' in query or 'abort' in query or 'stop' in query:
             speak('okay')
-            speak('Bye Rithu, have a good day.')
+            speak("Bye Tricky, have a good day!")
             sys.exit()
 
         elif 'hello' in query:
-            speak('Hello Rithu')
+            speak('Hello Tricky')
 
         elif"thanks"in query or 'thank you'in query or 'thank'in query:
-            speak("you're welcome Rithu!")
+            speak("you're welcome Tricky!")
 
         elif 'bye' in query:
-            speak('Bye Rithu, have a good day.')
+            speak('Bye Tricky, have a good day.')
             sys.exit()
 
         else:
@@ -159,7 +141,8 @@ if name == 'Rithu':
                     speak(results)
 
             except:
-                chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-                webbrowser.get(chrome_path).open('www.google.com')
+                speak("sorry, I couldnt find that. Here are the results on google.")
+                brave_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+                webbrowser.get(brave_path).open(f'https://www.google.com/search?q={query}')
 
-        speak('Next Query Rithu!')
+        speak('Next Query Tricky!')
